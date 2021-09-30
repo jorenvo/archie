f55c1abf02ea0440e881b0506b60eacf7a402c0e
Joren Van Onder <joren@jvo.sh>
2021-09-29 20:21:05 -0700

    Add gocui and play around a bit

--- archie.go
+++ archie.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/jroimartin/gocui"
 	"io"
 	"log"
 	"os"
@@ -10,25 +11,77 @@ import (
 	"unicode"
 )
 
-func speedRead(s string) {
+func getMainView(g *gocui.Gui) *gocui.View {
+	view, err := g.View("hello")
+	if err != nil {
+		log.Fatalf("Error getting view: %s\n", err)
+	}
+	return view
+}
+
+func writeWord(word string) func(*gocui.Gui) error {
+	return func(g *gocui.Gui) error {
+		view := getMainView(g)
+		view.Clear()
+		fmt.Fprintf(view, "%s", word)
+		return nil
+	}
+}
+
+func speedRead(g *gocui.Gui, s string) {
 	word := ""
 	for _, c := range s {
 		if unicode.IsSpace(c) {
-			fmt.Printf("%s\n", word)
+			g.Update(writeWord(word))
 			word = ""
-			time.Sleep(100 * time.Millisecond)
+			time.Sleep(500 * time.Millisecond)
 		} else {
 			word = word + string(c)
 		}
 	}
 }
 
-func main() {
+func mainReader(g *gocui.Gui) {
 	reader := bufio.NewReader(os.Stdin)
 	text, err := reader.ReadString('\000')
 	if err != io.EOF {
 		log.Fatalf("Could not read stdin: %s\n", err)
 	}
 
-	speedRead(text)
+	speedRead(g, text)
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	go mainReader(g)
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jorenvo/archie
 
 go 1.16
+
+require github.com/jroimartin/gocui v0.5.0
