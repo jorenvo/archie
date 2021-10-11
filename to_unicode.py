debc14f038e9f200e11505ea54b45e922c6af758
Joren Van Onder <joren@jvo.sh>
2021-10-10 19:53:51 -0700

    Add archie and a program to convert to unicode block elements

---
+++ to_unicode.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from PIL import Image
+
+# (a, b, c, d) is
+# a b
+# c d
+blocksToUnicode = {
+    (0, 0, 0, 0): " ",
+    (0, 0, 0, 1): "▗",
+    (0, 0, 1, 0): "▖",
+    (0, 0, 1, 1): "▄",
+    (0, 1, 0, 0): "▝",
+    (0, 1, 0, 1): "▐",
+    (0, 1, 1, 0): "▞",
+    (0, 1, 1, 1): "▟",
+    (1, 0, 0, 0): "▘",
+    (1, 0, 0, 1): "▚",
+    (1, 0, 1, 0): "▌",
+    (1, 0, 1, 1): "▙",
+    (1, 1, 0, 0): "▀",
+    (1, 1, 0, 1): "▜",
+    (1, 1, 1, 0): "▛",
+    (1, 1, 1, 1): "█",
+}
+
+assert len(set(blocksToUnicode.values())) == len(blocksToUnicode)
+
+im = Image.open("archie.png")
+pixels = im.load()
+
+frameWidth = 32
+frameHeight = 16
+
+for frame in range(4):
+    for row in range(1, 1 + frameHeight, 2):
+        for col in range(frameWidth * frame, frameWidth * frame + frameWidth, 2):
+            blocks = (
+                pixels[col, row],
+                pixels[col+1, row],
+                pixels[col, row+1],
+                pixels[col+1, row+1],
+            )
+            blocks = tuple(rgba[3] // 255 for rgba in blocks)
+
+            print(blocksToUnicode[blocks], end="")
+        print()
+    print()
